7f0eab109a905ee592e408fd68d3a7dbe2a1c5e5
NLGZ <edas96@naver.com>
2023-11-13 11:45:20 +0900

    Main"Animal Go~" 추가

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -12,6 +12,7 @@ public class Main {
 		System.out.println("Hello world");
 		System.out.println("Hello git");
 		
+		System.out.println("Animal Go~");		
 		new Dog().bark();
 		new Cat().jump();
 		new Lion().attack();
