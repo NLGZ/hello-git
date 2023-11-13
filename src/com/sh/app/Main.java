55c56f69ca6e597be81a71e5b242f04f3806d917
NLGZ <edas96@naver.com>
2023-11-13 10:33:37 +0900

    Dog#bark 추가

--- src/com/sh/app/Main.java
+++ src/com/sh/app/Main.java
@@ -1,10 +1,14 @@
 package com.sh.app;
 
+import com.sh.app.animal.Dog;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Hello git");
+		
+		new Dog().bark();
 	}
 
 }
